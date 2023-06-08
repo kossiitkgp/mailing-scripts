8187cde140dc36241b9bceaf89200a8d12dccc2a
proffapt <proffapt@pm.me>
2023-06-08 12:33:47 +0530

    feat: scale-able one-to-one

--- one-to-one.py
+++ one-to-one.py
@@ -3,16 +3,18 @@ import getpass
 import sys
 import os
 import base64
+import re
 from email.utils import formataddr
 from email.header import Header
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from templates.variable_mappings import variable_column_mapping
 
 # Help and number of argument passed checker
 if len(sys.argv) < 3:
-    print("USAGE: python3 one-to-one.py <template_file> <csv_file> (OPTIONAL)<other_variables>")
+    print("USAGE: python3 one-to-one.py <template_file> <csv_file> (OPTIONAL)<variables_with_same_value_for_all_mails>")
     print("python3 one-to-one.py selections/onboarding onboarding.csv")
     print("python3 one-to-one.py selections/onboarding onboarding.csv number_of_applicants='250+'")
     sys.exit(1)
@@ -25,14 +27,12 @@ template_file = "./templates/" + sys.argv[1]
 csv_file = "./csv/" + sys.argv[2]
 signature_file = "./templates/signature"
 
-# Getting subject
-with open(template_file, "r") as file:
-    subject = file.readline().strip()
-# Getting mail body
+# Getting subject and mail body
 lines = []
 with open(template_file, "r") as file:
-    lines = file.readlines()[2:]  # Slice the list starting from index 2 (line number 3)
-email_body = "".join(lines)
+    subject_template = file.readline().strip()
+    lines = file.readlines()[1:]  # Slice the list starting from index 2 (line number 3)
+email_body_template = "".join(lines)
 # Getting signature
 with open(signature_file) as file:
     signature = file.read()
@@ -56,7 +56,14 @@ def send_message(service, user_id, message):
     except Exception as e:
         print(f"An error occurred while sending the message: {e}")
 
-def main(subject, email_body, signature):
+def fill_variables(content, variables):
+    for variable, value in variables.items():
+        placeholder = "{" + variable + "}"
+        content = content.replace(placeholder, value)
+
+    return content 
+
+def main(subject_template, email_body_template, signature):
     creds = None
 
     if os.path.exists("token.json"):
@@ -75,32 +82,35 @@ def main(subject, email_body, signature):
 
     service = build("gmail", "v1", credentials=creds)
     
-    # Getting extra variables if required by the template
-    if len(sys.argv) > 3:
+    # Getting extra static variables(those which are same for all mails) if required by the template
+    if len(sys.argv) > 2:
         variables = {}
         for arg in sys.argv[3:]:
             variable, value = arg.split("=")
             variables[variable] = value
             
-        email_body = fill_variables(email_body, variables)
-        subject = fill_variables(subject, variables)
+        email_body_template = fill_variables(email_body_template, variables)
+        subject_template = fill_variables(subject_template, variables)
 
-    with open(csv_file, newline="") as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header row
+    with open(csv_file, newline="") as file:
+        reader = csv.DictReader(file)
 
         for row in reader:
-            email = row[1]
-            name = row[2]
+            email = row.get("Email")
+            
+            # Getting unique variables values
+            variable_placeholders = re.findall(r"\{(\w+)\}", email_body_template)
+            required_columns = set(variable_column_mapping.get(var) for var in variable_placeholders)
 
+            variables = {}
+            for variable, column in variable_column_mapping.items():
+                if column in required_columns:
+                    variables[variable] = row.get(column, "")
+
+            email_body = fill_variables(email_body_template, variables)
+            subject = fill_variables(subject_template, variables)
+            
             sender = "admin@kossiitkgp.org"
-            if mail_body == "task":
-                taskURL = row[7]
-                deadline = sys.argv[3]
-                emailBody = emailContents.getTaskMail(name, taskURL, deadline)
-            elif mail_body == "onboarding":
-                number_of_participants = sys.argv[3]
-                emailBody = emailContents.getOnboardingMail(name, number_of_participants)
             email_content = email_body + signature
             message = create_message(sender, email, subject, email_content)
             send_message(service, "me", {"raw": message})
@@ -109,4 +119,4 @@ def main(subject, email_body, signature):
     print("Script execution completed.")
 
 if __name__ == "__main__":
-    main(subject, email_body, signature)
+    main(subject_template, email_body_template, signature)
---
+++ templates/variable_mappings.py
@@ -0,0 +1,9 @@
+# Variable mappings
+# This file contains variables used in templates
+# mapped with the column names in csv files
+# Keep it updated.
+
+variable_column_mapping = {
+    "name": "Name",
+    "taskURL": "Tentative Task that you want to allot"
+}
