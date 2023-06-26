7fa87ad150212a40ba28d51b61cce9f184f07746
proffapt <proffapt@pm.me>
2023-06-27 02:32:26 +0530

    pre-feat: Dict List type variable mapping

--- templates/variable_mappings.py
+++ templates/variable_mappings.py
@@ -4,6 +4,7 @@
 # Keep it updated.
 
 variable_column_mapping = {
-    "name": "Name",
-    "taskURL": "Tentative Task that you want to allot"
+    "name": ["Name", "Name ", " Name", " Name ", "name", "name", " name", " name "],
+    "taskURL": ["Tentative Task that you want to allot"],
+    "email": ["Email", "Email ", " Email", " Email " "email", "email ", " email",  " email " "Email Address", "Email address"]
 }
