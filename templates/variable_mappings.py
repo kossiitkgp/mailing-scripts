0d42e8d85fe1981d32aefe8d7e56fbd8d8495b01
Karthikeya <yelisettikarthik0@gmail.com>
2024-02-10 13:59:14 +0530

    Missing commas between strings

--- templates/variable_mappings.py
+++ templates/variable_mappings.py
@@ -6,5 +6,5 @@
 variable_column_mapping = {
     "name": ["Name", "Name ", " Name", " Name ", "name", "name", " name", " name "],
     "taskURL": ["Tentative Task that you want to allot"],
-    "email": ["Email", "Email ", " Email", " Email " "email", "email ", " email",  " email " "Email Address", "Email address"]
+    "email": ["Email", "Email ", " Email", " Email ", "email", "email ", " email",  " email ", "Email Address", "Email address"]
 }
